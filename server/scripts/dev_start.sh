fe200a0a7dac45208dbad8fa4582f61e05e9ea01
chrishic <chris@chrishic.com>
2015-11-08 20:41:36 -0800

    Update start script to run service in background.

--- server/scripts/dev_start.sh
+++ server/scripts/dev_start.sh
@@ -2,8 +2,20 @@
 
 export NODE_ENV=development
 
+SERVICE_NAME=sur-price-me
+
+LOG_PATH="/var/log/chrishic/$SERVICE_NAME"
+LOG_FILENAME="$LOG_PATH/$SERVICE_NAME.log"
+
+# If there is an existing log file, save it (by renaming)
+if [ -f "$LOG_FILENAME" ]; then
+    # Get the current date/time stamp
+    d=$(date '+%y-%m-%d_%H-%M-%S')
+    mv $LOG_FILENAME $LOG_PATH/$SERVICE_NAME_$d.log
+fi
+
 pushd ..
 
-node index.js --name=SUR-PRICE-ME
+node index.js --name=$SERVICE_NAME >> $LOG_FILENAME 2>&1 &
 
 popd
